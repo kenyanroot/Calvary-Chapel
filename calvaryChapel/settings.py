7b6b0d145868f4203d625dc2cdc56193e90c34f5
anaco <ndedaanaco@gmail.com>
2021-01-23 23:36:44 +0300

    23/01/2020

--- calvaryChapel/settings.py
+++ calvaryChapel/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'calvaryChapel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -104,7 +104,7 @@ DATABASES = {
 #
 # import dj_database_url
 #
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
